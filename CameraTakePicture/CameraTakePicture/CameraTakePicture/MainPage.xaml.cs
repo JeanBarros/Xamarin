462eff787b55d6a93ee24b41b3f224815b772ac0
JeanBarros <ac.jbarros@gmail.com>
2018-08-05 23:51:24 -0300

    Projeto camera

---
+++ CameraTakePicture/CameraTakePicture/CameraTakePicture/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+
+namespace CameraTakePicture
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private async void UploadPictureButton_Clicked(object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("No upload", "Picking a photo is not suported.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync();
+            if (file == null)
+                return;
+
+            Image1.Source = ImageSource.FromStream(() => file.GetStream());
+        }
+
+        private async void TakePictureButton_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if(!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", "No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions{
+                SaveToAlbum = true,
+                Name = "teste.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            Image1.Source = ImageSource.FromStream(() => file.GetStream());
+        }
+    }
+}
